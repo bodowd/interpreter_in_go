69d705d4cc20d530629478a9b2c5cd9b70851238
bodowd <odowd.bing@gmail.com>
2022-06-08 22:44:38 +0200

    Left off at 1.4

--- Chapter1/src/monkey/token/token.go
+++ Chapter1/src/monkey/token/token.go
@@ -32,3 +32,20 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	// this is how you evaluate if a map contains a key in Go
+	// initialize two variables
+	// tok will receive either the value of "ident" from the map or an empty string
+	// ok will receive a bool that will be true if "ident" is in the map
+	// if true (ok is true), then it will execute the body of the if statemtn and tok will be local to that scope
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
